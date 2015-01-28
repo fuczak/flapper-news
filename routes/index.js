c9d3aecbd3efe3cf8064ae56a51c60a70122fcf5
fuczak <prus.damian@gmail.com>
2015-01-28 16:14:48 +0100

    upvoting comments works

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,17 @@ router.param('post', function(req, res, next, id) {
 	});
 });
 
+router.param('comment', function(req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec(function(err, comment) {
+		if(err) { return next(err); }
+		if(!comment) { return next(new Error("can't find comment")); }
+		req.comment = comment;
+		return next();
+	});
+});
+
 router.get('/posts/:post', function(req, res, next) {
 	req.post.populate('comments', function(err, post) {
 		res.json(post);
@@ -30,6 +41,13 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 	req.post.upvote(function(err, post) {
 		if(err) { return next(err); }
 		res.json(post);
+	});
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+	req.comment.upvote(function(err, comment) {
+		if(err) { return next(err); }
+		res.json(comment);
 	});
 });
 
