f928bdee925967fe3455fd9ed3339e323f3f0549
fuczak <prus.damian@gmail.com>
2015-01-28 10:01:45 +0100

    39/79 front-end fully working

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-'user strict';
-
-angular.module('flapperNews', ['ui-router'])
+angular.module('flapperNews', ['ui.router'])
 	.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 		$stateProvider
 			.state('home', {
 				url: '/home',
 				templateUrl: '/home.html',
 				controller: 'MainCtrl'
+			})
+			.state('posts', {
+				url: '/posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
 			});
 		$urlRouterProvider.otherwise('home');
 	}])
@@ -27,7 +30,11 @@ angular.module('flapperNews', ['ui-router'])
 			$scope.posts.push({ 
 				title: $scope.title,
 				link: $scope.link, 
-				upvotes: 0 
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+					{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
 			});
 			$scope.title ='';
 			$scope.link='';
@@ -35,4 +42,18 @@ angular.module('flapperNews', ['ui-router'])
 		$scope.incrementUpvotes = function (post) {
 			post.upvotes += 1;
 		};
-	}])	
+	}])
+	.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts) {
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function() {
+			if($scope.body === '') {
+				return ;
+			}
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = ''
+		};		
+	}])
